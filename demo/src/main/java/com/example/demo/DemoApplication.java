96b7320281b250ced2d020c01fb66a9f02b6118c
chenzhangyuan <654454048@qq.com>
2019-10-21 17:58:37 +0800

    上传gitbup代码修改1

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -31,7 +31,7 @@ public class DemoApplication {
     }
 
     @ResponseBody
-    @RequestMapping(value = "/hello2", method = RequestMethod.POST)
+    @RequestMapping(value = "/hello3", method = RequestMethod.POST)
     public String hello1(HttpServletRequest request){
         JSONObject resultJson = new JSONObject();
         try {
